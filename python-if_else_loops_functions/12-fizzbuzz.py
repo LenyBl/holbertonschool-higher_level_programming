420feb27e1cd9ac6928bb153e4f06082dc9e060e
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2026-01-13 13:38:50 +0000

    feat: implement fizzbuzz function to print numbers from 1 to 100 with FizzBuzz rules

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    """Print numbers from 1 to 100 with FizzBuzz rules."""
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
