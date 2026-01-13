a63099367c521ff37ee87d67f9d9891487ce147f
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2026-01-13 13:39:52 +0000

    fix: modify fizzbuzz function to print on the same line

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,10 +3,10 @@ def fizzbuzz():
     """Print numbers from 1 to 100 with FizzBuzz rules."""
     for num in range(1, 101):
         if num % 3 == 0 and num % 5 == 0:
-            print("FizzBuzz")
+            print("FizzBuzz", end="")
         elif num % 3 == 0:
-            print("Fizz")
+            print("Fizz", end="")
         elif num % 5 == 0:
-            print("Buzz")
+            print("Buzz", end="")
         else:
             print(num)
