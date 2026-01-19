234e56539e29306859be5a89a5045dfe0697e133
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2026-01-19 10:58:49 +0000

    fix: correct indexing and loop logic in divisible_by_2 function

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     new_list = []
-    for i in range(0, len(my_list) - 1):
-        if my_list[i] % 2 == 0:
-            new_list[i] = True
+    count = 0
+    for i in my_list:
+        if i % 2 == 0:
+            new_list[count] = True
         else:
-            new_list[i] = False
+            new_list[count] = False
+        count += count
     return new_list
