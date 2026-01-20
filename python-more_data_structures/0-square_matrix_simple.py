85359e9008e03bc0a22189cebbc4bd3f85a973dc
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2026-01-20 09:23:12 +0000

    fix: create a deep copy of the matrix in square_matrix_simple function

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = matrix[:]
+    new_matrix = [row[:] for row in matrix]
     idx = 0
     for i in range(0, len(matrix)):
         for j in matrix[i]:
