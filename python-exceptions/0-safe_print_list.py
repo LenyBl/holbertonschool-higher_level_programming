363d40b3394c3cb4780432ec4747a9724fac17a2
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2026-01-21 11:37:48 +0000

    refactor: optimize safe_print_list function for improved iteration and error handling

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    count = 0
-    for i in range(x):
-        try:
-            print(my_list[i], end="")
-            count += 1
-        except IndexError:
-            break
-    print()
-    return count
+    i = 0
+    for item in my_list:
+        if i < x:
+            try:
+                print(item, end='')
+                i += 1
+            except Exception:
+                break
+    print('')
+    return (i)
