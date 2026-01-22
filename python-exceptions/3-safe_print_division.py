f8ec57ac10ad522ba094be19e55bfb612a1e9d28
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2026-01-22 13:13:26 +0000

    fix: correct exception handling in safe_print_division function for zero division

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
     try:
         result = a / b
         return result
-    except ValueError:
+    except ZeroDivisionError:
         return None
     finally:
         print("{:d}".format(result))
