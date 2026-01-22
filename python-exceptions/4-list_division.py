c381dfc5f1cd3aac5c206c4887f6baca86af8e3e
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2026-01-22 14:25:44 +0000

    fix: add missing newline at end of file in list_division function

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,3 +1,18 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    
+    try:
+        result = []
+        for i in range(list_length):
+            try:
+                division = my_list_1[i] / my_list_2[i]
+            except ZeroDivisionError:
+                print("division by 0")
+                division = 0
+            except TypeError:
+                print("wrong type")
+                division = 0
+            result.append(division)
+        return result
+    except IndexError:
+        print("out of range")
+        return result 
