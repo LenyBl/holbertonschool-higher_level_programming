d3fc459240667ea8946b3407338d848b6ec9cbe8
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2026-01-23 08:38:29 +0000

    fix: refactor list_division function for improved error handling and readability feat: add initial matrix_divided module feat: add initial test file for matrix_divided module

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    try:
-        result = []
-        for i in range(list_length):
-            try:
-                division = my_list_1[i] / my_list_2[i]
-            except ZeroDivisionError:
-                print("division by 0")
-                division = 0
-            except TypeError:
-                print("wrong type")
-                division = 0
-            result.append(division)
-        return result
-    except IndexError:
-        print("out of range")
-        return result
+    result = []
+    for i in range(list_length):
+        res = 0
+        try:
+            res = (my_list_1[i] / my_list_2[i])
+        except ZeroDivisionError:
+            print("division by 0")
+        except TypeError:
+            print("wrong type")
+        except IndexError:
+            print("out of range")
+        finally:
+            result.append(res)
+    return result
