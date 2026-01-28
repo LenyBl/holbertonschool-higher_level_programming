67746980390989dd240ab6e56396dfea428899d1
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2026-01-28 08:41:31 +0000

    feat: add module docstring to Square class for improved documentation

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,3 +1,9 @@
+"""
+This module defines a Square class with a size attribute.
+"""
+
+
 class Square:
+    """A class representing a square with a size attribute."""
     def __init__(self, size=0):
         self.size = size
