b0c6bc14d607878b12db29013c24d3e5e92ee9ae
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2026-02-04 09:05:33 +0000

    feat: add abstract base class Animals with Dog and Cat implementations

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,34 @@
+from abc import ABC, abstractmethod
+"""
+Module defining an abstract base class Animals
+with an abstract method sound.
+"""
+
+
+class Animals(ABC):
+    """ Abstract base class for animals with an abstract method sound."""
+    @abstractmethod
+    def sound(self):
+        """ Abstract method to be implemented by subclasses to produce
+        animal sound."""
+        pass
+
+
+class Dog(Animals):
+    """ Dog class that inherits from Animals and implements the sound
+    method."""
+    def sound(self):
+        """
+        Returns the sound made by the dog.
+        """
+        return "Bark"
+
+
+class Cat(Animals):
+    """ Cat class that inherits from Animals and implements the sound
+    method."""
+    def sound(self):
+        """
+        Returns the sound made by the cat.
+        """
+        return "Meow"
