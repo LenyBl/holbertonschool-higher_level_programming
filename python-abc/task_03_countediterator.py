5b3a1e0a9f564a16fbf666f7ba7060c7ddd8d754
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2026-02-04 13:32:48 +0000

    feat: implement CountedIterator class to count iterations over an iterable

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Module defining a CountedIterator class that wraps an iterable
+and counts the number of iterations.
+"""
+
+
+class CountedIterator:
+    """
+    An iterator that counts the number of iterations.
+    Wraps around any iterable and provides a method to get the count
+    of iterations performed.
+    """
+    def __init__(self, iterable):
+        """
+        Initialize the CountedIterator with an iterable.
+        args:
+            iterable: An iterable to wrap.
+        """
+        self._iterable = iter(iterable)
+        self._count = 0
+
+    def get_count(self):
+        """
+        Get the number of iterations performed.
+        returns:
+            int: The number of iterations.
+        """
+        return self._count
+
+    def __next__(self):
+        """
+        Get the next item from the iterable and increment the count.
+        returns:
+            The next item from the iterable.
+        raises:
+            StopIteration: When the iterable is exhausted.
+        """
+        self._count += 1
+        return next(self._iterable)
