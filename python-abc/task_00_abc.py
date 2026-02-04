87cf269a81c2a09a43ce02b578400eaf8c8d77a2
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2026-02-04 09:12:28 +0000

    fix: correct class name from Animals to Animal for consistency in task_00_abc.py

--- python-abc/task_00_abc.py
+++ python-abc/task_00_abc.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """
-Module defining an abstract base class Animals
+Module defining an abstract base class Animal
 with an abstract method sound.
 """
 
 from abc import ABC, abstractmethod
 
 
-class Animals(ABC):
-    """Abstract base class for animals with an abstract method sound."""
+class Animal(ABC):
+    """Abstract base class for animal with an abstract method sound."""
 
     @abstractmethod
     def sound(self):
@@ -16,17 +16,27 @@ class Animals(ABC):
         pass
 
 
-class Dog(Animals):
-    """Dog class that inherits from Animals and implements sound."""
+class Dog(Animal):
+    """Dog class that inherits from Animal and implements sound."""
 
     def sound(self):
         """Return the sound made by the dog."""
         return "Bark"
 
 
-class Cat(Animals):
-    """Cat class that inherits from Animals and implements sound."""
+class Cat(Animal):
+    """Cat class that inherits from Animal and implements sound."""
 
     def sound(self):
         """Return the sound made by the cat."""
         return "Meow"
+
+
+bobby = Dog()
+garfield = Cat()
+
+print(bobby.sound())
+print(garfield.sound())
+
+animal = Animal()
+print(animal.sound())
