fbbcd862edae7781e38a27204b81ed37a5acae19
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2026-02-11 08:48:02 +0000

    feat: implement save_to_json_file and load_from_json_file functions

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,34 +1,15 @@
 #!/usr/bin/python3
-
+"""
+Module file that contains the function save_to_json_file
+"""
 import json
 
 
 def save_to_json_file(my_obj, filename):
-    
-    json_text = json.loads(my_obj)
+    """
+    Writes an Object to a text file, using a JSON representation
+    """
+    object_to_string = json.dumps(my_obj)
+    json_text = json.loads(object_to_string)
     with open(filename, "w") as f:
-        f.write(json_text)
-
-filename = "my_list.json"
-my_list = [1, 2, 3]
-save_to_json_file(my_list, filename)
-
-filename = "my_dict.json"
-my_dict = { 
-    'id': 12,
-    'name': "John",
-    'places': [ "San Francisco", "Tokyo" ],
-    'is_active': True,
-    'info': {
-        'age': 36,
-        'average': 3.14
-    }
-}
-save_to_json_file(my_dict, filename)
-
-try:
-    filename = "my_set.json"
-    my_set = { 132, 3 }
-    save_to_json_file(my_set, filename)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+        f.write(str(json_text))
