e8ab33b095b3ac11e0420ba6c691ed6244207a12
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2026-02-11 08:52:04 +0000

    refactor: simplify save_to_json_file function by removing unnecessary conversions

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -9,7 +9,5 @@ def save_to_json_file(my_obj, filename):
     """
     Writes an Object to a text file, using a JSON representation
     """
-    object_to_string = json.dumps(my_obj)
-    json_text = json.loads(object_to_string)
     with open(filename, "w") as f:
-        f.write(str(json_text))
+        json.dump(my_obj, f)
