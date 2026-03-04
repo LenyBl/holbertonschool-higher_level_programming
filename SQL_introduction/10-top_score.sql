79d06189474b23d28bbb8059b38d8544f5af9937
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2026-03-04 10:32:46 +0000

    feat: update SQL query to retrieve all records with the top score from second_table

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,2 +1,8 @@
 -- Write a SQL query to find the top score from the second_table and return all the records with that score.
-SELECT (score, name) FROM second_table WHERE score = (SELECT MAX(score) FROM second_table);
+SELECT 
+    score
+    name
+FROM
+    second_table
+ORDER BY 
+    score DESC
