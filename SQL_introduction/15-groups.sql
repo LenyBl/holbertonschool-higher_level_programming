2444d5848a97e0499c5ffc0bd39a21a1945f3675
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2026-03-04 10:45:31 +0000

    fix: correct alias name from 'student_count' to 'number' in SQL query

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,6 +1,6 @@
 -- Write a SQL query to count the number of students for each score in the second_table and
 -- return the results ordered by score in descending order.
-SELECT score, COUNT(*) as student_count
+SELECT score, COUNT(*) as number
 FROM second_table
 GROUP BY score
 ORDER BY score DESC;
