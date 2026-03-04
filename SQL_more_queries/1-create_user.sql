aabec6c3f0d6ad1becf2705ccb8be177e9359def
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2026-03-04 13:39:40 +0000

    fix: specify host for user_0d_1 in CREATE USER statement

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- Create a new user named user_0d_1 with the password user_0d_1_pwd
-CREATE USER IF NOT EXISTS user_0d_1 IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
