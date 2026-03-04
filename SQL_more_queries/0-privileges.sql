e5412cdea464a63292193be71275d94d8963f713
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2026-03-04 13:36:29 +0000

    feat: update SQL script to show user privileges using SHOW GRANTS

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,3 @@
 -- Show the privileges of the users
-SELECT grantee, privilege_type
-FROM information_schema.user_privileges
-WHERE grantee IN ('user_0d_1@localhost', 'user_0d_2@localhost');
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
