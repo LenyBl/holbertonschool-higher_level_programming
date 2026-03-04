a185597c5d60859762cf0f576652c37b3f548459
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2026-03-04 13:40:57 +0000

    feat: add SQL script to create user_0d_2 with SELECT privileges

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- Create a new user named user_0d_2 with the password user_0d_2_pwd and give this user only SELECT privileges
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+USE hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON *.* TO 'user_0d_2'@'localhost';
