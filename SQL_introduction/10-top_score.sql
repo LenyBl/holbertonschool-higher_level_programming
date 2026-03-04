1bbc1d6db6379d929dbb3ba6ffe777c07a508d45
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2026-03-04 10:30:01 +0000

    feat: update SQL query to select score and name from second_table

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
 -- Write a SQL query to find the top score from the second_table and return all the records with that score.
-SELECT * FROM second_table ORDER BY score DESC
+SELECT (score, name) FROM second_table ORDER BY score DESC
