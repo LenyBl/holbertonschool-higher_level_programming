0bdef88c511ad06612ae7823db1f84a38bf7809a
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2026-03-04 10:34:49 +0000

    feat: add SQL query to find top score records from second_table

---
+++ SQL_introduction/11-best_score.sql
@@ -0,0 +1,10 @@
+-- Write a SQL query to find the top score from the second_table and return all the records with that score.
+SELECT
+    score,
+    name
+FROM
+    second_table
+WHERE
+    score <= 10
+ORDER BY
+    score DESC
