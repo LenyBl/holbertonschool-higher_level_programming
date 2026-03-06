1b195f16a0f816a663ffd43caac97267e3f8f336
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2026-03-06 09:26:26 +0000

    fix: correct table name in SQL query for genre_id of shows

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,6 @@
 -- Write a SQL query to find the genre_id of each show. The result should include
 -- the show title and genre_id, and be ordered by show id in ascending order.
-SELECT tv_shows.title, tv_shows_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.genre_id IS NULL
