afbfd7e6dccfed91ca5dd92dfa65b9a5d746a9e7
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2026-03-06 09:02:17 +0000

    fix: select specific columns in SQL query for cities in California

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- Write a SQL query to find all the cities in California. Use a subquery to find the state_id for California.
-SELECT * FROM cities
+SELECT id, name FROM cities
 WHERE state_id = (SELECT id FROM states WHERE name = 'California')
-ORDER BY cities.id ASC;
+ORDER BY id ASC;
