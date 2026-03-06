058e6f213aa1d19d3d4bff7e5c74ae023026aab8
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2026-03-06 09:04:56 +0000

    feat: add SQL query to find all cities in California using JOIN

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,6 @@
+-- Write a SQL query to find all the cities in California. Use a JOIN to connect the cities and states tables.
+SELECT cities.id, cities.name, states.name
+FROM cities
+JOIN states ON cities.state_id = states.id
+WHERE states.name = 'California'
+ORDER BY cities.id ASC;
