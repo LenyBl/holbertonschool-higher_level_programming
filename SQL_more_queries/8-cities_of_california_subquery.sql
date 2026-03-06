6787ada104f8e86ae9faae1832485d515b8fdda7
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2026-03-06 09:01:08 +0000

    fix: add ORDER BY clause to SQL query for cities in California

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,4 @@
 -- Write a SQL query to find all the cities in California. Use a subquery to find the state_id for California.
 SELECT * FROM cities
-WHERE state_id = (SELECT id FROM states WHERE name = 'California');
+WHERE state_id = (SELECT id FROM states WHERE name = 'California')
+ORDER BY cities.id ASC;
