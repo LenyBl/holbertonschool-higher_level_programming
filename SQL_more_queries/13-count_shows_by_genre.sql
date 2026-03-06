4a46a510909da9d725522837eac72827c1ed2a28
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2026-03-06 09:29:14 +0000

    feat: add SQL query to count the number of shows for each genre ordered by show count

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- Write a SQL query to count the number of shows for each genre. The result should
+-- include the genre name and the number of shows, and be ordered by the number of
+-- shows in descending order.
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC;
