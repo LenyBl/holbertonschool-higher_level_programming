71b63c2f7d9ef3b427946768f791f1b4ac378fa2
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2026-03-06 09:24:35 +0000

    feat: add SQL query to find genre_id of each show with title ordered by show id

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,7 @@
+-- Write a SQL query to find the genre_id of each show. The result should include
+-- the show title and genre_id, and be ordered by show id in ascending order.
+SELECT tv_shows.title, tv_shows_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
