81b92389efd271783cecc9a3ee73377716d9d9b0
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2026-03-18 22:00:45 +0000

    fix: improve output for non-integer command line argument in 5-to_integer.js

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,3 +1,7 @@
 #!/usr/bin/node
 const argv1 = process.argv[2];
-console.log(parseInt(argv1));
+if (parseInt(argv1)) {
+  console.log('My number: ' + parseInt(argv1));
+} else {
+  console.log('Not a number');
+}
